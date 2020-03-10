a340a98780e6df9e9602ef7049f024cc604f0f65
trichimtrich <trichimtrich@gmail.com>
2020-03-10 22:53:52 +0800

    fix google map url + parser

--- covi/logic.js
+++ covi/logic.js
@@ -1,7 +1,19 @@
 function parseGoogleMap(url) {
-    data = /@([-.\d]+),([-.\d]+),/.exec(url);
-    lat = parseFloat(data[1]);
-    lng = parseFloat(data[2]);
+    var data, lat, lng;
+    data = /!3d([-.\d]+)!4d([-.\d]+)/.exec(url);
+    if (data) {
+        lat = parseFloat(data[1]);
+        lng = parseFloat(data[2]);
+    } else {
+        data = /@([-.\d]+),([-.\d]+),/.exec(url);
+        if (data) {
+            lat = parseFloat(data[1]);
+            lng = parseFloat(data[2]);
+        } else {
+            console.log("[Error] Cant parse this url");
+            console.log(url);
+        }
+    }
     return [lat, lng];
 }
 
