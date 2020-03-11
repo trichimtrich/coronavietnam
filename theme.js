75a07bc920edca09afe513b5201cea878441a444
trichimtrich <trichimtrich@gmail.com>
2020-03-12 00:29:21 +0800

    relocation files, add credit to please my fat friend o.o

--- theme.js
+++ theme.js
@@ -25,7 +25,7 @@ _themes.MapboxStreets = {
 
 // CartoDB_DarkMatter
 var tileCartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a> | dev <a href="https://github.com/trichimtrich/trichimtrich.github.io">trichimtrich</a>',
     subdomains: 'abcd',
     maxZoom: 19
 });
